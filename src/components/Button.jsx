778ad2b02ea0da4a962ecc9777b7182e058f19b3
Kyle Steger <kylesteger@gmail.com>
2026-07-22 10:58:23 -0400

    MOAR PADDING

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Button = ({ label, onClick }) => {
   return (
-    <button onClick={onClick} style={{ padding: "8px 16px" }}>
+    <button onClick={onClick} style={{ padding: "12px 20px" }}>
       {label}
     </button>
   );
