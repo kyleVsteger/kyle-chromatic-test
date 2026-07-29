aa6fba663e708531464b61aabe499ba33aab72e1
Kyle Steger <kylesteger@gmail.com>
2026-07-29 15:20:18 -0400

    MORE PADDING

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Button = ({ label, onClick }) => {
   return (
-    <button onClick={onClick} style={{ padding: "20px 40px" }}>
+    <button onClick={onClick} style={{ padding: "30px 50px" }}>
       {label}
     </button>
   );
